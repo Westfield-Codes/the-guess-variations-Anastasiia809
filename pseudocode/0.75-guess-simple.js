36751e56e0480a75207aeaa04d3c0cbaffe5b70f
Anastasiia809 <anastasiia208805@student-schoolsofwestfield.org>
2025-03-10 14:28:20 +0000

    done guess simpler

--- pseudocode/0.75-guess-simple.js
+++ pseudocode/0.75-guess-simple.js
@@ -1,14 +1,17 @@
 /* Simple Guess Program */
-// create variable called guess, set to 0
-
-// create variable called answer and store a random integer 1-100
-
-// display the answer for testing purposes only
-
-// set guess equal to prompt asking user to "Guess a number, 1-100"
-
-// if guess equals answer display "Correct!" 
-
-// or if guess is greater than answer display "Too high!" 
-
-// or display "Too low!" 
+function Guesser() {
+    // create variable called guess, set to 0
+        var guess = 0;
+    // create variable called answer and store a random integer 1-100
+        var answer = Math.floor(Math.random()*100+1);
+    // display the answer for testing purposes only
+        alert(answer);
+    // set guess equal to prompt asking user to "Guess a number, 1-100"
+        var guess = prompt("Guess a number, 1-100");
+    // if guess equals answer display "Correct!" 
+        if (guess == answer) alert("Correct!");
+    // or if guess is greater than answer display "Too high!" 
+        else if (guess > answer) alert("Too high!");
+    // or display "Too low!" 
+        else alert("Too low!");
+    }
