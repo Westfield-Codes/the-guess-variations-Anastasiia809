7f3ae168999c82155eb6660f4a02dc1a5f63de6c
Anastasiia809 <anastasiia208805@student-schoolsofwestfield.org>
2025-03-11 10:40:19 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-/* Simple Guess Program */
-// define function guessGame
-function guessGame(){
-  // generate a random integer 1-100
-  let answer = Math.floor(Math.random()*100+1);
-  // display the integer for testing purposes only
-  alert(answer);
-  // create guess and set equal to 0 
-  let guess = 0;
-  // loop while guess is not equal to answer or to "q"
-  while (guess != answer || guess != "q") {
-    // set guess equal to asking user to "Guess a number, 1-100"
-    guess = prompt("Guess a number, 1-100");
-    // if guess equals "q" display "Quitter!"
-    if (guess == "q") alert("Quitter!");
-    // or if guess equals answer display "Correct!" 
-    else if (guess == answer) alert("Correct!");
-    // or if guess is greater than answer display "Too high!" 
-    else if (guess > answer) alert("Too high!");
-    // or if guess is less than answer display "Too low!" 
-    else if (guess < answer) alert("Too low!");
-    // or say "Bad input"
-    else alert("Bad input!");
-  // end the loop
-  }
-// end the function
-}
+/* Variable and Alert Katas */
+// These katas do not involve functions and are very basic. 
+
+/* COLOR KATAS */
+
+/* Var Alert */
+// make a string variable for a color, set it equal to your favorite
+var color = "green";
+// alert the message" "My favorite color is " plus the color
+alert("My favourite color is: " + color);
+
+/* Var Alert Prompt */
+// make a string variable for color, prompt the user for their favorite
+// alert color " is a nice color!"
+
+/* Var Alert Prompt Conditional */
+// make a variable for color, prompt the user for their favorite
+// if user enters "black" alert "Black is not really a color." 
+// otherwise alert color " is a nice color!"
